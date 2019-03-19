ad08f7c7b9a1fc87001a308c49d2b1524bb84c34
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-19 14:17:23 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def dictionary
+dictionary = {"hello" => "hi",
+              "to" => "2",
+              "two" => "2",
+              "too" => "2",
+              "for" => "4",
+              "For" => "4",
+              "four" => "4",
+              "be" => "b",
+              "you" => "u",
+              "at" => "@",
+              "and" => "&"}
+end
+
+def word_substituter(tweet)
+  array = []
+  array = tweet.split(" ")
+  array.each_with_index do |word, index|
+    if dictionary.keys.include?(word)
+      array.insert(index, dictionary[word])
+      array.delete_at(index + 1)
+end
+end
+new_tweet = ""
+new_tweet = array.join(" ")
+puts new_tweet
+new_tweet
+end
